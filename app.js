ff2df4c2a6002d9f081527bad01d62225f0a7517
elijahkems <49605011+elijahkems@users.noreply.github.com>
2022-03-29 14:11:43 -0500

    added starting styles and animation

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 gsap;
 
-let mainAnimation = gsap.timeline();
+let mainAnimation = gsap.timeline({ delay: 0.5 });
+let box1 = document.querySelectorAll(".square")[0];
+let box2 = document.querySelectorAll(".square")[1];
+mainAnimation.to(box1, { top: "-50%", ease: "back" });
+mainAnimation.to(box2, { top: "-50%", ease: "back" });
